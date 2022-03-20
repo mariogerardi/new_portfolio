c248a500538d3a435013a46ac8ae01bb728460b6
mariogerardi <mgerardi@vassar.edu>
2022-03-20 02:10:21 -0400

    Added styling base for header.

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,9 +2,27 @@ import React from 'react';
 import '../App.css';
 
 function Header() {
-  return (
-    <h1 className="App-header">Mario Gerardi</h1>
-  );
+    return (
+        <div className="header">
+            <div className="profile">
+                <img className="portrait" src={require('../assets/portrait.png')}/>
+            </div>
+            <div className="titleContainer">
+                <h1 className="letter" id="start">M</h1>
+                <h1 className="letter">a</h1>
+                <h1 className="letter">r</h1>
+                <h1 className="letter">i</h1>
+                <h1 className="letter">o</h1>
+                <h1 className="letter">G</h1>
+                <h1 className="letter">e</h1>
+                <h1 className="letter">r</h1>
+                <h1 className="letter">a</h1>
+                <h1 className="letter">r</h1>
+                <h1 className="letter">d</h1>
+                <h1 className="letter" id="end">i</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
