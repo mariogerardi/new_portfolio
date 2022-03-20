48d93f90c46bb6ae5db6a5b9694ec57b8ea356f1
mariogerardi <mgerardi@vassar.edu>
2022-03-19 22:40:05 -0400

    Initialized very basic layout.

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function Hello() {
+  return (
+    <Header />
+  );
+}
+
+export default Hello;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <h1 className="App-header">Mario Gerardi</h1>
+  );
+}
+
+export default Header;
