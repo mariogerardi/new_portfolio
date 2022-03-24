0df246095e7db96a099078ffb20aeb717869bee1
mariogerardi <mgerardi@vassar.edu>
2022-03-24 15:50:23 -0400

    Finished animations. Including the golden easter egg hehe.

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -24,80 +24,103 @@ function Header() {
     const [flip, setFlip] = useState(0)
     const [bounce, setBounce] = useState(0)
     const [spiral, setSpiral] = useState(0)
-    const [orbit, setOrbit] = useState(0)
     const [up, setUp] = useState(0)
     const [down, setDown] = useState(0)
     const [mirror, setMirror] = useState(0)
     const [resume, setResume] = useState(0)
+    const [superflip, setSuperflip] = useState(0)
+    const [finale, setFinale] = useState(0)
+    const [count, setCount] = useState(0)
 
     function makeMeSpin() {
         if (spin === 0) {
             setSpin(1);
             setToggle1(1);
+            setCount(count+1)
         } else {
             setSpin(0);
             setToggle1(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function doTheWave() {
         if (wave === 0) {
             setWave(1);
             setToggle2(1);
+            setCount(count+1)
         } else {
             setWave(0);
             setToggle2(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function moveIt() {
         if (migrate === 0) {
             setMigrate(1)
             setToggle3(1);
+            setCount(count+1)
         } else {
             setMigrate(0);
             setToggle3(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function timeToStretch() {
         if (stretch === 0) {
             setStretch(1)
             setToggle4(1);
+            setCount(count+1)
         } else {
             setStretch(0);
             setToggle4(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function doAFlip() {
         if (flip === 0) {
             setFlip(1)
             setToggle5(1);
+            setCount(count+1)
         } else {
             setFlip(0);
             setToggle5(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function bouncyBois() {
         if (bounce === 0) {
             setBounce(1)
             setToggle6(1);
+            setCount(count+1)
         } else {
             setBounce(0);
             setToggle6(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function spiralLikeMe() {
         if (spiral === 0) {
             setSpiral(1)
             setToggle7(1);
+            setCount(count+1)
         } else {
             setSpiral(0);
             setToggle7(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function shiftInRhythm() {
@@ -105,42 +128,92 @@ function Header() {
             setUp(1)
             setDown(1)
             setToggle8(1);
+            setCount(count+1)
         } else {
             setUp(0);
             setDown(0);
             setToggle8(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function reverseReverse() {
         if (mirror === 0) {
             setMirror(1)
             setToggle9(1);
+            setCount(count+1)
         } else {
             setMirror(0);
             setToggle9(0);
+            setCount(count-1)
         }
+        counter();
     }
 
     function hireMeIDareYou() {
         if (resume === 0) {
             setResume(1)
             setToggle10(1);
+            setCount(count+1)
         } else {
             setResume(0);
             setToggle10(0);
+            setCount(count-1)
+        }
+        counter();
+    }
+
+    function flipButCooler() {
+        if (superflip === 0) {
+            setSuperflip(1)
+            setToggle11(1);
+            setCount(count+1)
+        } else {
+            setSuperflip(0);
+            setToggle11(0);
+            setCount(count-1)
+        }
+        counter();
+    }
+
+    function grandFinale() {
+        if (finale === 0) {
+            setFinale(1)
+            setToggle12(1);
+            setCount(count+1)
+        } else {
+            setFinale(0);
+            setToggle12(0);
+            setCount(count-1)
+        }
+        counter();
+    }
+
+    function counter() {
+        if (count <= 11) {
+            return (
+                <img 
+                    className="portrait"
+                    src={require('../assets/avatar.png')}
+                    spin={spin}
+                />
+            )
+        } else {
+            return (
+                <img 
+                    className="portrait"
+                    src={require('../assets/avatar-gold.png')}
+                    spin={spin}
+                />
+            )
         }
     }
 
     return (
         <div className="header">
             <div className="profile">
-                <img 
-                    className="portrait"
-                    src={require('../assets/avatar.png')}
-                    spin={spin}
-                    flip={flip}
-                />
+                {counter()}
             </div>
             <div 
                 className="titleContainer" 
@@ -152,7 +225,7 @@ function Header() {
                     toggle={toggle1}
                     down={down}
                 >
-                    <h1>M</h1>
+                    <h1 superflip={superflip}>M</h1>
                 </button>
                 <button 
                     className="letter"
@@ -168,7 +241,7 @@ function Header() {
                     onClick={moveIt}
                     flip={flip}
                 >
-                    <h1>r</h1>
+                    <h1 superflip={superflip}>r</h1>
                 </button>
                 <button 
                     className="letter"
@@ -193,7 +266,7 @@ function Header() {
                     onClick={bouncyBois}
                     flip={flip}
                 >
-                    <h1>G</h1>
+                    <h1 finale={finale}>G</h1>
                 </button>
                 <button 
                     className="letter"
@@ -209,7 +282,7 @@ function Header() {
                     onClick={shiftInRhythm}
                     up={up}
                 >
-                    <h1>r</h1>
+                    <h1 superflip={superflip}>r</h1>
                 </button>
                 <button 
                     className="letter"
@@ -226,12 +299,13 @@ function Header() {
                     mirror={mirror}
                     onClick={hireMeIDareYou}
                 >
-                    <h1>r</h1>
+                    <h1 superflip={superflip}>r</h1>
                 </button>
                 <button 
                     className="letter"
                     toggle={toggle11}
                     down={down}
+                    onClick={flipButCooler}
                 >
                     <h1>d</h1>
                 </button>
@@ -239,13 +313,15 @@ function Header() {
                     className="letter" 
                     id="end"
                     toggle={toggle12}
+                    onClick={grandFinale}
+                    finale={finale}
                 >
                     <h1 bounce={bounce}>i</h1>
                 </button>
             </div>
             <h2 className="migrate" migrate={migrate}><em>Front-End Developer | Filmmaker</em></h2>
             <h3 className="stretch" stretch={stretch}>Nice to meet you.</h3>
-            <a className="resume" href={pdf} target="_blank" resume={resume}>Résumé</a>
+            <a className="resume" href={pdf} target="_blank" resume={resume} count={count}>Résumé</a>
         </div>
     );
 }
